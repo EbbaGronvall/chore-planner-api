42df84e4a0d46b7e59fa5b1d1d8348663a91c726
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-11-21 22:54:27 +0000

    Profile serializer updated to include household slug

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -8,7 +8,8 @@ from tasks.serializers import TaskSerializer
 class ProfileSerializer(serializers.ModelSerializer):
     member = serializers.ReadOnlyField(source='member.username')
     is_member = serializers.SerializerMethodField()
-    household = serializers.SerializerMethodField()
+    household_name = serializers.SerializerMethodField()
+    household_slug = serializers.SerializerMethodField()
     tasks = serializers.SerializerMethodField()
 
     def get_tasks(self, obj):
@@ -19,13 +20,19 @@ class ProfileSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.member
     
-    def get_household(self, obj):
+    def get_household_name(self, obj):
         if obj.household:
             return obj.household.name
         return "No household"
 
+    def get_household_slug(self, obj):
+        if obj.household:
+            return obj.household.slug
+        return None
+    
+
     class Meta:
         model = Profile
         fields = [
-            'id', 'member', 'household', 'role', 'image', 'tasks', 'is_member',
+            'id', 'member', 'household_name', 'household_slug', 'role', 'image', 'tasks', 'is_member',
         ]
